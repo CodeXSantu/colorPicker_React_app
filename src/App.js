83932f50c40339d313e55f1b1da9a0226ee2bfbd
santu-github <kumarsantos7033@gmail.com>
2021-07-23 15:11:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {SketchPicker} from 'react-color'
+import React,{useState} from "react";
 
 function App() {
+  const [color,setColor] = useState('blue')
+  const [hidden,setHidden] = useState(false)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:color}} className="App">
+      <div className="container">
+      {
+        hidden ?
+        <SketchPicker className="color-picker" transp color={color} onChange={(updateColor) =>setColor(updateColor.hex)} /> : null
+      }
+        
+        <button style={{color:color}} onClick ={() => setHidden(!hidden)}>
+          {
+            hidden ? 'Close Color Picker' : 'Open Color Picker'
+          }
+        </button>
+      </div>
     </div>
   );
 }
